ce97f08aa67c3b703911d3019c6b2ba88c701c75
Max Braitmaiere <max@hexadite.com>
2016-04-13 15:31:02 +0300

    version bump to 1.1.18.2-hexadite

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 
 setup_kwargs = dict(
     name='txwinrm',
-    version='1.1.18.1-hexadite',
+    version='1.1.18.2-hexadite',
     description='Asynchronous Python WinRM client',
     long_description=open('README.rst').read(),
     license='See LICENSE file',
